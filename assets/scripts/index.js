13f27c01998907d0c28c8bbad61e8a33bf96626d
Théodore Cadet <cadetthe@gmail.com>
2022-02-23 11:42:36 +0100

    second commit

---
+++ assets/scripts/index.js
@@ -0,0 +1,23 @@
+let btnVideo = document.getElementsByClassName("teaserBtn");
+let videoTeaser = document.getElementById("videoTeaser");
+let theVideo = document.getElementById("theVideo");
+var b = document.querySelector("button");
+// btnVideo.onclick = showVideo();
+
+function showVideo() {
+
+    theVideo.setAttribute("controls", "");
+    theVideo.removeAttribute("autoplay");
+    theVideo.removeAttribute("muted");
+    theVideo.removeAttribute("loop");
+    theVideo.muted = false;
+    // videoTeaser.style.opacity = 1;
+    // videoTeaser.style.zIndex = 11;
+    videoTeaser.style.height = "100vh";
+    videoTeaser.style.width = "100vw";
+
+    if (theVideo.requestFullscreen) {
+        theVideo.requestFullscreen();
+    }
+
+}
