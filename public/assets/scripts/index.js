39eeccf0ccf0fe85b847f963a1818d4d350ec4d0
Théodore Cadet <cadetthe@gmail.com>
2022-03-01 17:33:59 +0100

    new form

--- public/assets/scripts/index.js
+++ public/assets/scripts/index.js
@@ -21,3 +21,33 @@ function showVideo() {
     }
 
 }
+
+const form = document.getElementById("theFormOxyclash");
+const formInputs = document.getElementById("theFormOxyclash").elements;
+// var axios = require('axios');
+
+// var qs = require('qs');
+
+form.addEventListener("submit", (e) => {
+    var myHeaders = new Headers();
+    myHeaders.append("Content-Type", "application/x-www-form-urlencoded");
+
+    var urlencoded = new URLSearchParams();
+    urlencoded.append("name", formInputs[0].value);
+    urlencoded.append("gamerTag", formInputs[1].value);
+    urlencoded.append("eMail", formInputs[2].value);
+    urlencoded.append("dateOdBirth", formInputs[3].value);
+    urlencoded.append("validated", "false");
+
+    var requestOptions = {
+        method: 'POST',
+        headers: myHeaders,
+        body: urlencoded,
+        redirect: 'follow'
+    };
+
+    fetch("http://localhost:80/sub", requestOptions)
+        .then(response => response.text())
+        .then(result => console.log(result))
+        .catch(error => console.log('error', error));
+})
